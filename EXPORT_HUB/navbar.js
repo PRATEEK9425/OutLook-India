ff41dbf7f2b9b523aa35989256f35f18264fb157
sdibyanshu <sdibyanshu>
2022-08-25 00:51:46 +0530

    Navbar day-1 added

---
+++ EXPORT_HUB/navbar.js
@@ -0,0 +1,24 @@
+  function navbar(){
+   
+    
+                    
+             return   `<div> <a href="signup.html">HOME </a></div>
+                 <div><a href="">OUTLOOK </a></div>
+                 <div><a href="">BUSINESS</a></div>
+                 <div><a href="">MONEY</a></div>
+                 <div><a href="">CRYPTO CORNER</a></div>
+                 <div><a href="">TRAVEL</a></div>
+                 <div><a href="">SPORTS</a></div>
+                 <div><a href="">VIDEOS </a></div>
+                 <div><a href="">ENTERTAINMENT</a></div>
+                 <div><a href="">PHOTOS</a></div>
+                 <div><a href="">MAGAZINE</a></div>
+                 <div><a href="">ESG BHARAT</a></div>
+                 <div><a href="">OTHERS</a></div>
+                 <div><a href="">AGRITECH</a></div>`
+
+    
+  }
+
+  export default navbar()
+  
